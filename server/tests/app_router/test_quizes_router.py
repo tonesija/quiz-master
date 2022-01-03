0d5590bc0b6294cd39cde3ec1d989579dfa4e596
Tonio Ercegović <tonio.ercegovic@fer.hr>
2022-01-03 16:35:00 +0100

    added '/api' to endpoint UTs

--- server/tests/app_router/test_quizes_router.py
+++ server/tests/app_router/test_quizes_router.py
@@ -7,9 +7,9 @@ from tests.conftest import test_db_session
 
 class TestQuizesRouter:
     def test_get_quizes(self, client, seed_quizes_to_you):
-        """Test the "/my/quizzes" endpoint."""
+        """Test the "api//my/quizzes" endpoint."""
 
-        res = client.get("/my/quizzes")
+        res = client.get("api//my/quizzes")
 
         assert res.status_code == status.HTTP_200_OK
 
@@ -19,19 +19,19 @@ class TestQuizesRouter:
         assert seed_quizes_to_you[0]["name"] == res_json[0]["name"]
 
     def test_create_quiz(self, client, seed_quizes_to_you, quiz_2):
-        """Test the "/my/quizzes" POST endpoint."""
+        """Test the "api//my/quizzes" POST endpoint."""
 
         with test_db_session() as db:
             num_quizes_before = db.query(Quiz).count()
 
-        res = client.post("/my/quizzes", json=quiz_2)
+        res = client.post("api//my/quizzes", json=quiz_2)
 
         assert res.status_code == status.HTTP_201_CREATED
         with test_db_session() as db:
             assert db.query(Quiz).count() == num_quizes_before + 1
 
     def test_get_one_quiz(self, client, quiz_1, seed_you):
-        """Test the "/my/quizzes/{quiz_id}" endpoint."""
+        """Test the "api//my/quizzes/{quiz_id}" endpoint."""
 
         id = seed_you
 
@@ -41,58 +41,58 @@ class TestQuizesRouter:
             db.commit()
             quiz_id = quiz_db.id
 
-        res = client.get(f"/my/quizzes/{quiz_id}")
+        res = client.get(f"api//my/quizzes/{quiz_id}")
         res_json = res.json()
 
         assert res.status_code == status.HTTP_200_OK
         assert quiz_1["name"] == res_json["name"]
 
-        res = client.get("/my/quizzes/10000")
+        res = client.get("api//my/quizzes/10000")
 
         assert res.status_code == status.HTTP_404_NOT_FOUND
 
     def test_update_quiz(self, client, seed_quizes_to_you, quiz_2):
-        """Test the "/my/quizzes/{quiz_id}" PUT endpoint."""
+        """Test the "api//my/quizzes/{quiz_id}" PUT endpoint."""
 
         with test_db_session() as db:
             quiz_before = db.query(Quiz).first()
 
-        res = client.put(f"/my/quizzes/{quiz_before.id}", json=quiz_2)
+        res = client.put(f"api//my/quizzes/{quiz_before.id}", json=quiz_2)
 
         assert res.status_code == status.HTTP_200_OK
         with test_db_session() as db:
             quiz_after = db.query(Quiz).first()
             assert quiz_after.name == quiz_2["name"]
 
-        res = client.put(f"/my/quizzes/10000", json=quiz_2)
+        res = client.put(f"api//my/quizzes/10000", json=quiz_2)
 
         assert res.status_code == status.HTTP_404_NOT_FOUND
 
     def test_delete_quiz(self, client, seed_quizes_to_you):
-        """Test the "/my/quizzes/{quiz_id}" DELETE endpoint."""
+        """Test the "api//my/quizzes/{quiz_id}" DELETE endpoint."""
 
         with test_db_session() as db:
             quiz_before = db.query(Quiz).first()
             num_quiz_before = db.query(Quiz).count()
 
-        res = client.delete(f"/my/quizzes/{quiz_before.id}")
+        res = client.delete(f"api//my/quizzes/{quiz_before.id}")
 
         assert res.status_code == status.HTTP_200_OK
         with test_db_session() as db:
             assert db.query(Quiz).count() == num_quiz_before - 1
 
-        res = client.delete(f"/my/quizzes/10000")
+        res = client.delete(f"api//my/quizzes/10000")
 
         assert res.status_code == status.HTTP_404_NOT_FOUND
 
     def test_get_one_quizes_questions(self, client, seed_quizes_to_you):
-        """Test the "/my/quizzes/{quiz_id}/questions" endpoint."""
+        """Test the "api//my/quizzes/{quiz_id}/questions" endpoint."""
 
         with test_db_session() as db:
             quiz_db = db.query(Quiz).first()
             questions = quiz_db.questions
 
-        res = client.get(f"/my/quizzes/{quiz_db.id}/questions")
+        res = client.get(f"api//my/quizzes/{quiz_db.id}/questions")
         res_json = res.json()
 
         assert res.status_code == status.HTTP_200_OK
@@ -101,12 +101,12 @@ class TestQuizesRouter:
             question_dict["outer_text"] for question_dict in res_json
         ]
 
-        res = client.get("/my/quizzes/10000/questions")
+        res = client.get("api//my/quizzes/10000/questions")
 
         assert res.status_code == status.HTTP_404_NOT_FOUND
 
     def test_add_question_to_quiz(self, client, seed_quizes_to_you, question_3):
-        """Test the "/my/quizzes/{quiz_id}/add-question" POST endpoint."""
+        """Test the "api//my/quizzes/{quiz_id}/add-question" POST endpoint."""
 
         with test_db_session() as db:
             quiz_db = db.query(Quiz).first()
@@ -121,7 +121,8 @@ class TestQuizesRouter:
             num_questions_before = len(quiz_db.questions)
 
         res = client.post(
-            f"/my/quizzes/{quiz_db.id}/add-question", json={"question_id": question_id}
+            f"api//my/quizzes/{quiz_db.id}/add-question",
+            json={"question_id": question_id},
         )
 
         assert res.status_code == status.HTTP_200_OK
@@ -131,7 +132,7 @@ class TestQuizesRouter:
             assert len(quiz_db.questions) == num_questions_before + 1
 
         res = client.post(
-            "/my/quizzes/10000/add-question", json={"question_id": question_id}
+            "api//my/quizzes/10000/add-question", json={"question_id": question_id}
         )
 
         assert res.status_code == status.HTTP_404_NOT_FOUND
